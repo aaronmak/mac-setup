1b1614cfa00c438a87505771a37cfd4d969b9c3c
Aaron Mak <aaronmakks@gmail.com>
2019-05-07 23:05:00 +0800

    Add neovim

--- brew.sh
+++ brew.sh
@@ -38,6 +38,7 @@ brew install wget --with-iri
 
 # Install more recent versions of some OS X tools.
 brew install vim --override-system-vi
+brew install neovim
 brew install homebrew/dupes/grep
 brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
