9350a6bc47b49244e369d16c9694efb0df84f0c2
Aaron Mak <aaronmakks@gmail.com>
2020-10-10 14:44:10 +0800

    Cleanup unused brews

--- brew.sh
+++ brew.sh
@@ -47,67 +47,26 @@ brew install grep
 brew install openssh
 brew install screen
 
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xz
-
 # Install other useful binaries.
 brew install ack
 brew install bat
 brew install bitwarden-cli
 brew install cmake
-brew install dark-mode
 brew install entr
 brew install fzf
 "$(brew --prefix)"/opt/fzf/install --key-bindings --completion --xdg
 brew install gh
 brew install git
-brew install git-flow
-brew install git-lfs
 brew install hub
 brew install imagemagick
 brew install jq
 brew install kubectx
-brew install kubernetes-helm
 brew install lua
 brew install lynx
 brew install mosh
-brew install p7zip
 brew install pandoc
 brew install peco
-brew install pigz
-brew install pkg-config libffi
-brew install pv
 brew install reattach-to-user-namespace
-brew install rename
-brew install rhino
 brew install ripgrep
 brew install shellcheck
 brew install speedtest-cli
@@ -116,7 +75,6 @@ brew install tig
 brew install tldr
 brew install tree
 brew install watch
-brew install webkit2png
 brew install wifi-password
 brew install z
 brew install zopfli
