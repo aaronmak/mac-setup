070ed5a9d2aaf5244f8060a6fced243cacbd88d6
Aaron Mak <im@arnmk.com>
2021-11-05 17:37:32 +0800

    Moved this to dotfiles instead

--- nvim.sh
+++ nvim.sh
@@ -9,16 +9,6 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # Install neovim as a replacement for vim
 brew install neovim
 
-# Create required required directories and files for neovim configs
-mkdir -p ~/.config/nvim
-touch "$HOME/.config/nvim/init.vim"
-
-cat <<EOT > "$HOME/.config/nvim/init.vim"
-set runtimepath^=~/.vim runtimepath+=~/.vim/after
-let &packpath = &runtimepath
-source ~/.vimrc
-EOT
-
 # Install vim-plug as plugin manager
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
